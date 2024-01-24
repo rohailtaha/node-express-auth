2b82b8b3683c828391c634703351562b24d5b4ec
Rohail Taha <taharohail77@gmail.com>
2024-01-24 21:19:59 +0500

    feat: support google and github login

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { getDatabase } = require('./lib/database');
 const { body, validationResult } = require('express-validator');
 const LocalStrategy = require('passport-local');
+const GithubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
 const app = express();
 const { hashPassword, verifyPassword } = require('./lib/utils');
@@ -31,6 +33,120 @@ app.use(
   })
 );
 
+passport.use(
+  new LocalStrategy(async function verify(username, password, done) {
+    const db = await getDatabase();
+    try {
+      const user = await db.collection('users').findOne({
+        email: username,
+      });
+      if (!user) return done(null, false);
+
+      // check if the user logged in previously using OAuth provider.
+      if (user.authenticationProvider) {
+        return done(null, false);
+      }
+
+      const passwordVerified = await verifyPassword(password, user.password);
+      if (passwordVerified) {
+        done(null, { _id: user._id.toString() });
+      } else {
+        done(null, false);
+      }
+    } catch (err) {
+      done(err, null);
+    }
+  })
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      // refresh token is undefined
+      const db = await getDatabase();
+
+      const user = await db.collection('users').findOne({
+        email: profile._json.email,
+      });
+
+      // save the user in db if does not already exist.
+      if (!user) {
+        await db.collection('users').insertOne({
+          email: profile._json.email,
+          name: profile._json.name,
+          authenticationProvider: profile.provider,
+        });
+      }
+
+      // If the user was previously logged in using another strategy than update
+      // the authenticationProvider
+      if (user && user.authenticationProvider !== profile.provider) {
+        await db.collection('users').updateOne(
+          {
+            email: profile._json.email,
+          },
+          {
+            $set: {
+              name: profile._json.name,
+              authenticationProvider: profile.provider,
+            },
+            $unset: { password: '' },
+          }
+        );
+      }
+
+      return done(null, { _id: profile._json.id.toString() });
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      const db = await getDatabase();
+
+      const user = await db.collection('users').findOne({
+        email: profile._json.email,
+      });
+
+      if (!user) {
+        await db.collection('users').insertOne({
+          email: profile._json.email,
+          name: profile._json.name,
+          authenticationProvider: profile.provider,
+        });
+      }
+
+      if (user && user.authenticationProvider !== profile.provider) {
+        await db.collection('users').updateOne(
+          {
+            email: profile._json.email,
+          },
+          {
+            $set: {
+              name: profile._json.name,
+              authenticationProvider: profile.provider,
+            },
+            $unset: { password: '' },
+          }
+        );
+      }
+
+      return done(null, { _id: profile._json.sub });
+    }
+  )
+);
+
 passport.serializeUser((user, done) => {
   // This function will only be called on login (we login when we signup and login)
   // The value provided to callback will be set in the session.passport.user property
@@ -42,26 +158,6 @@ passport.deserializeUser(async (id, done) => {
   // the value provided to callback will be set to req.user
   done(null, { _id: id });
 });
-
-passport.use(
-  new LocalStrategy(async function verify(username, password, cb) {
-    const db = await getDatabase();
-    try {
-      const user = await db.collection('users').findOne({
-        email: username,
-      });
-      if (!user) return cb(null, false);
-      const passwordVerified = await verifyPassword(password, user.password);
-      if (passwordVerified) {
-        cb(null, { _id: user._id.toString() });
-      } else {
-        cb(null, false);
-      }
-    } catch (err) {
-      cb(err, null);
-    }
-  })
-);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -146,10 +242,43 @@ app.post(
   })
 );
 
+app.get(
+  '/auth/github',
+  passport.authenticate('github', {
+    scope: ['user:email'],
+  })
+);
+
+app.get(
+  '/auth/github/callback',
+  passport.authenticate('github', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+  })
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+  })
+);
+
 app.get('/logout', (req, res) => {
   req.logout(err => {
     if (err) return res.status(500).end();
-    res.redirect('/login');
+    req.session.destroy(err => {
+      if (err) return res.status(500).end();
+      return res.redirect('/login');
+    });
   });
 });
 
