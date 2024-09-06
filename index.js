340ccb6c2ee055c7752b6ed0298472c2502f04c3
rohailtaha <taharohail77@gmail.com>
2024-09-06 22:48:56 +0500

    feat: add cors

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const { authRouter } = require('./auth-router');
 const { passport } = require('./passport-config');
 const { isGuest } = require('./middlewares/guest');
 const { isAuthenticated } = require('./middlewares/authentication');
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -56,7 +58,7 @@ app.get('/about', isAuthenticated, (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
