0533078dbcd12b000a38f64aed5d67ee2fda1772
Rohail Taha <taharohail77@gmail.com>
2024-09-06 19:11:28 +0000

    refactor: remove comments

--- index.js
+++ index.js
@@ -11,21 +11,6 @@ const { isAuthenticated } = require('./middlewares/authentication');
 const cors = require('cors');
 
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept'
-//   );
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   console.log('reached');
-//   if (req.method === 'OPTIONS') {
-//     console.log('reached2');
-//     return res.sendStatus(200);
-//   }
-//   next();
-// });
 
 app.use(express.json());
 app.use(
