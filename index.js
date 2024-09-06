c7687046da9bf28abc69884585adb82fa38c2d09
rohailtaha <taharohail77@gmail.com>
2024-09-06 23:52:09 +0500

    update cors setting

--- index.js
+++ index.js
@@ -10,22 +10,22 @@ const { isGuest } = require('./middlewares/guest');
 const { isAuthenticated } = require('./middlewares/authentication');
 const cors = require('cors');
 
-// app.use(cors());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header('Access-Control-Allow-Credentials', 'true');
-  console.log('reached');
-  if (req.method === 'OPTIONS') {
-    console.log('reached2');
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   );
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   console.log('reached');
+//   if (req.method === 'OPTIONS') {
+//     console.log('reached2');
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 app.use(express.json());
 app.use(
